308bde0e4d8d77c0e6289d075e4eaa15533616ba
Cristian Montes <cmontesmujica@gmail.com>
2021-12-07 16:57:40 -0700

    added structure of home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Home from './views/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello Motto </h1>
+        <Home />
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello Motto/i);
+  const linkElement = screen.getByText(/Welcome to the Kickball League!/i);
   expect(linkElement).toBeInTheDocument();
 });
